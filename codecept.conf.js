65dc58b5590d990d6a6e86bedb34a494635120c9
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-04-27 22:32:53 -0300

    Initial commit

---
+++ codecept.conf.js
@@ -0,0 +1,36 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Appium: {
+      platform: 'Android',
+      app: '../app_qazando/app/app-release.apk',
+      desiredCapabilities: {
+        appPackage: 'com.qazandoapp',
+        appActivity: 'MainActivity',
+        device: 'Pixel2',
+        platformVersion:'9'
+      }
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'app_qazando',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
